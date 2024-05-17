3a6fdf6cac8949102ce60c95f6fdfdc46cb45d8c
harsha-0110 <112392323+harsha-0110@users.noreply.github.com>
2024-05-17 14:24:51 +0530

    Update run.sh
    Modified run.sh to support fw1100 by default

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 echo "PPPWN - Designed for JIO Fiber Routers"
 interface=eth3.1
-firmware=900
-stage1=/home/PPPWN-JIO-Router-main/stage1_900.bin
-stage2=/home/PPPWN-JIO-Router-main/stage2_900.bin
+firmware=1100
+stage1=/home/PPPWN-JIO-Router-main/stage1_1100.bin
+stage2=/home/PPPWN-JIO-Router-main/stage2_1100.bin
 cd /home/PPPWN-JIO-Router-main
 chmod +x ./pppwn
 
