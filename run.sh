19dbda13356b1ec6b82ddcf7c7ee2ae23bfe6d08
harsha-0110 <112392323+harsha-0110@users.noreply.github.com>
2024-05-18 22:45:57 +0530

    Update run.sh
    Updated the code to make the LED to blink in blue color while the pppwn is running and solid blue after execution is complete.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "PPPWN - Designed for JIO Fiber Routers"
 interface=eth3.1
 firmware=1100
@@ -6,4 +8,32 @@ stage2=/home/PPPWN-JIO-Router-main/stage2_1100.bin
 cd /home/PPPWN-JIO-Router-main
 chmod +x ./pppwn
 
+# Function to keep the LED in blue fast blink
+keep_led_blue_fast_blink() {
+    while true; do
+        ledctl1 ALL off
+        ledctl1 BLUE fastBlink
+        sleep 1
+    done
+}
+
+# Trap to ensure LED is turned off when the script exits
+cleanup() {
+    kill $led_blink_pid
+    ledctl1 ALL off
+    ledctl1 BLUE on
+}
+
+# Execute the cleanup function when the script exits or is interrupted
+trap cleanup EXIT
+
+# Start the LED blinking in the background
+keep_led_blue_fast_blink &
+led_blink_pid=$!
+
+# Execute the pppwn command
 ./pppwn --interface $interface --fw $firmware --stage1 $stage1 --stage2 $stage2 --auto-retry
+
+# Stop the LED blinking loop (cleanup function will handle this)
+cleanup
+trap - EXIT
